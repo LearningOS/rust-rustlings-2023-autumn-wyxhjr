5083f65c8540a708d9ce1481c3fbb0c9303a2426
oslab <you@example.comgit>
2023-10-11 00:58:05 +0800

    msg

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,12 +14,13 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
 struct Team {
+    name: String,
     goals_scored: u8,
     goals_conceded: u8,
 }
@@ -39,20 +40,19 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
-        let score = scores.entry(team_1_name.clone()).or_insert(Team {
-            name: team_1_name,
-            goals_scored: 0,
-            goals_conceded: 0,
-        });
-(*score).goals_scored += team_1_score;
-(*score).goals_conceded += team_2_score;
-let score = scores.entry(team_2_name.clone()).or_insert(Team {
-    name: team_2_name,
-    goals_scored: 0,
-    goals_conceded: 0,
-});
-(*score).goals_scored += team_2_score;
-(*score).goals_conceded += team_1_score;
+         if let Some(xe) = scores.get_mut(&team_1_name){
+            let pztemp1 = Team{name:team_1_name.clone(),goals_scored:team_1_score+xe.goals_scored,goals_conceded:team_2_score+xe.goals_conceded};
+            *xe = pztemp1;
+            
+        }else{
+            scores.entry(team_1_name.clone()).or_insert(Team{name:team_1_name,goals_scored:team_1_score,goals_conceded:team_2_score});
+        }
+        if let Some(xee) = scores.get_mut(&team_2_name){
+            let pztemp2 = Team{name:team_2_name.clone(),goals_scored:team_2_score+xee.goals_scored,goals_conceded:team_1_score+xee.goals_conceded};
+            *xee = pztemp2;
+        }else{
+            scores.entry(team_2_name.clone()).or_insert(Team{name:team_2_name,goals_scored:team_2_score,goals_conceded:team_1_score});
+        }
     }
     scores
 }
