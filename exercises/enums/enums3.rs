cae7acae09dbad84f20dc99596827891a3b2ca1a
oslab <you@example.comgit>
2023-10-10 11:15:16 +0800

    update

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -9,6 +9,10 @@
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
+     Move {Point},
+    Echo(String),
+    ChangeColor(u8, u8, u8),
+    Quit
 }
 
 struct Point {
@@ -43,6 +47,14 @@ impl State {
         // variants
         // Remember: When passing a tuple as a function argument, you'll need
         // extra parentheses: fn function((t, u, p, l, e))
+          Message::ChangeColor(r, g, b) => {
+        self.change_color((r, g, b));
+    }
+        Message::Echo(s) => self.echo(s),
+        Message::Quit => self.quit(),
+        Message::Move(pt) => {
+            self.move_position(pt);
+        }
     }
 }
 
